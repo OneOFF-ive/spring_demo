054264211b0eca81fff5798f30800fe55b669108
Wu <2321141691@qq.com>
2023-03-16 23:18:51 +0800

    新开一个项目，用于学习spring boot及相关组件

---
+++ src/test/java/com/five/spring_demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.five.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
