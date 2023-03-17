394fe638da296ac45607aaba31da07fb55cb2200
Wu <2321141691@qq.com>
2023-03-17 17:11:57 +0800

    配置swagger2

--- src/main/java/com/five/spring_demo/controller/HelloController.java
+++ src/main/java/com/five/spring_demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.five.spring_demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
