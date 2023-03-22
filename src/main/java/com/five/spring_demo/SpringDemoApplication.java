ebea6e1a0fe3e16dd37fb1a109a865764e33531f
Wu <2321141691@qq.com>
2023-03-22 14:56:34 +0800

    瑞吉外卖项目基本配置

--- src/main/java/com/five/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/five/spring_demo/SpringDemoApplication.java
@@ -1,15 +1,18 @@
 package com.five.spring_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.five.spring_demo.mapper")
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class, args);
+        log.info("项目启动成功");
     }
 
 }
---
+++ src/main/java/com/five/spring_demo/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.five.spring_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
