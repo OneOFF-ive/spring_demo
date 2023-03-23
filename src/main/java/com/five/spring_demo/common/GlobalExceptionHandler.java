2b4e8b7e4ccdf33047d238b85f84e3fe79eada38
Wu <2321141691@qq.com>
2023-03-23 10:41:23 +0800

    实现了简单的全局异常处理器

---
+++ src/main/java/com/five/spring_demo/common/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.five.spring_demo.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Component
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@Slf4j
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    R<String> exceptHandler(SQLIntegrityConstraintViolationException e) {
+        log.error(e.getMessage());
+        return R.error("失败了");
+    }
+
+}
