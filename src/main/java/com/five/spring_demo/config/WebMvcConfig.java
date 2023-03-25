2996849da421571bb10a47e7279ce647f5ff3c09
Wu <2321141691@qq.com>
2023-03-25 19:32:05 +0800

    拓展了MVC消息转换器，解决了Long数据的精度问题，将Long转变为String

--- src/main/java/com/five/spring_demo/config/WebMvcConfig.java
+++ src/main/java/com/five/spring_demo/config/WebMvcConfig.java
@@ -1,12 +1,18 @@
 package com.five.spring_demo.config;
 
+import com.five.spring_demo.common.JacksonObjectMapper;
 import com.five.spring_demo.interceptor.LoginCheckInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.util.List;
 
+@Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
@@ -30,4 +36,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
                         "/backend/**", "/front/**");
     }
 
+    /**
+     * 扩展MVC消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器");
+        // 创建消息转换器
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        // 设置对象转换器,使用Jackson将Java对象转换成Json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        // 将自定义消息转换器放入MVC消息转换器集合中
+        // 索引0，优先使用自定义的消息转换器
+        converters.add(0, messageConverter);
+    }
 }
