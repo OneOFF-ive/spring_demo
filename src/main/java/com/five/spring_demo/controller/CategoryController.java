ec6b3bdf0f871663e71f34fe151a28f41d8f56b3
Wu <2321141691@qq.com>
2023-03-28 20:12:58 +0800

    实现了Category的分页查询和保存

--- src/main/java/com/five/spring_demo/controller/CategoryController.java
+++ src/main/java/com/five/spring_demo/controller/CategoryController.java
@@ -1,7 +1,41 @@
 package com.five.spring_demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.five.spring_demo.common.R;
+import com.five.spring_demo.entity.Category;
+import com.five.spring_demo.entity.Employee;
+import com.five.spring_demo.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    R<String> generateCategory(@RequestBody Category category) {
+        log.info("category:{}", category);
+        if (categoryService.save(category)) {
+
+            return R.success("新增分类成功");
+        }
+        return R.error("新增分类失败");
+    }
+
+    @GetMapping("/page")
+    R<Page<Category>> getPage(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize) {
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
 }
--- src/main/java/com/five/spring_demo/entity/Category.java
+++ src/main/java/com/five/spring_demo/entity/Category.java
@@ -16,18 +16,17 @@ public class Category implements Serializable {
 
     private Long id;
 
-
     //类型 1 菜品分类 2 套餐分类
     private Integer type;
-
 
     //分类名称
     private String name;
 
-
     //顺序
     private Integer sort;
 
+    //是否删除
+    private Integer isDeleted;
 
     //创建时间
     @TableField(fill = FieldFill.INSERT)
@@ -47,9 +46,5 @@ public class Category implements Serializable {
     //修改人
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateUser;
-
-
-    //是否删除
-    private Integer isDeleted;
 
 }
