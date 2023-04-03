42da136f360e030aaa80edddda47e3488ba1e6f4
Wu <2321141691@qq.com>
2023-04-03 21:08:09 +0800

    实现了文件下载功能

--- src/main/java/com/five/spring_demo/controller/CommonController.java
+++ src/main/java/com/five/spring_demo/controller/CommonController.java
@@ -3,11 +3,17 @@ package com.five.spring_demo.controller;
 import com.five.spring_demo.common.R;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
@@ -20,7 +26,7 @@ public class CommonController {
     private String basePath;
 
     @PostMapping("/upload")
-    R<String> upload(MultipartFile file) {
+    public R<String> upload(MultipartFile file) {
 
         String originalFileName = file.getOriginalFilename();
         String suffix = Objects.requireNonNull(originalFileName).substring(originalFileName.lastIndexOf("."));
@@ -38,5 +44,26 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.error("上传失败");
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(basePath + name));
+
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
