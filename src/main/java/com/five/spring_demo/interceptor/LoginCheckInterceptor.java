d45c312fe0c714540dee13332ead935097db08e6
Wu <2321141691@qq.com>
2023-04-11 21:40:07 +0800

    优化拦截器

--- src/main/java/com/five/spring_demo/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/five/spring_demo/interceptor/LoginCheckInterceptor.java
@@ -5,6 +5,7 @@ import com.five.spring_demo.common.BaseContext;
 import com.five.spring_demo.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,5 +32,11 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
         log.info("拦截未通过:{}", request.getRequestURI());
         return false;
+    }
+
+    @Override
+    public void afterCompletion(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, @Nullable Exception ex) {
+        log.info("请求结束:{}", request.getRequestURI());
+        BaseContext.removeCurrentId();
     }
 }
